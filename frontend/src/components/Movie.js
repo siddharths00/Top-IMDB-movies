78f2bfe58c6f3730a9904723ade4066c5e1960cc
Siddharth S <45630336+siddharths00@users.noreply.github.com>
2022-01-18 15:16:36 +0530

    Update Movie.js

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import "../App.css";
-// import { movies } from '../../backend/movies';
 
 export const Movie = (props) => {
 
@@ -12,22 +11,12 @@ export const Movie = (props) => {
         return await fetch("http://localhost:5000/movie")
         .then((response) => response.json())
         .then((data) => {
-            let newEdit = { ...ans };
-            console.log("==========",data);
-            newEdit = data;
+            let newEdit = { ...ans };newEdit = data;
             data=data.slice(0,limit);
-            setAns(data);
-            // setAns(data,()=>{
-                
-                
-            // });
-            
+            setAns(data);            
         });}
         useEffect(() => {
-            // Update the document title using the browser API
-            // setMovies({});
             fetchData();
-            // console.log(movies)
             let temp=ans.slice(0,limit);
             setAns(temp);
             mov=ans.map((data, key) => {
@@ -41,20 +30,22 @@ export const Movie = (props) => {
               })
           },[limit]);
         
-          function handleChange(e){
-            setLimit(e.target.value);
+
+          function handleSubmit(e){
+            e.preventDefault();
+            setLimit(e.target[0].value);
             console.log(limit)
-          };
+          }
 
   return (
     <>
       <div className="stock-container">
-          {/* {console.log("%%%%%%%%%%%%",typeof(ans),ans)} */}
+          <form  onSubmit={handleSubmit}>
           <input
           type="text"
-          value={limit}
-          onChange={handleChange}
         />
+        <button type="submit">Submit</button>
+        </form>
           <table>
         {ans.map((data, key) => {
                 return (
